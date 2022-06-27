eef1be9c3c61aa22d1740370278cdba94b296092
mafumaful <2993821152@qq.com>
2022-06-27 22:04:46 +0800

    note

--- src/my_c_action/src/my_c_actionclient_node.cpp
+++ src/my_c_action/src/my_c_actionclient_node.cpp
@@ -3,25 +3,31 @@
 #include <future>
 #include <memory>
 #include <string>
-#include <sstream>
 
+// 包含action文件
 #include "my_action/action/count.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
+// 设定命名空间my_c_actionclient_node
 namespace my_c_actionclient_node
 {
+    // 新建一个类，名称为CountActionClient，继承rclcpp::Node节点
     class CountActionClient : public rclcpp::Node
     {
     public:
+        // Count使用别名my_action::action::Count
+        // GoalHandleCount使用别名rclcpp_action::ClientGoalHandle<Count>
         using Count = my_action::action::Count;
         using GoalHandleCount = rclcpp_action::ClientGoalHandle<Count>;
 
-        explicit CountActionClient(const rclcpp::NodeOptions &options)
+        // 构造函数
+        CountActionClient(const rclcpp::NodeOptions &options)
             : Node("Count_action_client", options)
         {
+            // 创建一个客户，绑定Count的服务名称
             this->client_ptr_ = rclcpp_action::create_client<Count>(
                 this->get_node_base_interface(),
                 this->get_node_graph_interface(),
@@ -29,64 +35,86 @@ namespace my_c_actionclient_node
                 this->get_node_waitables_interface(),
                 "Count");
 
+            // 利用句柄创建一个定时器,绑定回调函数sent_goal()
             this->timer_ = this->create_wall_timer(
                 std::chrono::milliseconds(500),
                 std::bind(&CountActionClient::send_goal, this));
         }
 
+        // 发送目标
         void send_goal()
         {
+            // 使用std::placeholders的命名空间
             using namespace std::placeholders;
-
+            // 取消计时器
             this->timer_->cancel();
 
+            // 等待服务器，准备好返回true或者超时就返回false
             if (!this->client_ptr_->wait_for_action_server())
             {
+                // 打印错误日志
                 RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
                 rclcpp::shutdown();
             }
 
+            // 创建一个Goal的实例
             auto goal_msg = Count::Goal();
+            // 将目标赋值为10
             goal_msg.goal_num = 10;
 
+            // 发送数据输出打印
             RCLCPP_INFO(this->get_logger(), "Sending goal");
 
+            // 创建一个发送目标配置的实例
             auto send_goal_options = rclcpp_action::Client<Count>::SendGoalOptions();
+            // 绑定目标应答的回调函数goal_response_callback
             send_goal_options.goal_response_callback =
                 std::bind(&CountActionClient::goal_response_callback, this, _1);
+            // 绑定目标反馈的回调函数feedback_callback
             send_goal_options.feedback_callback =
                 std::bind(&CountActionClient::feedback_callback, this, _1, _2);
+            // 绑定结果返回的回调函数result_callback
             send_goal_options.result_callback =
                 std::bind(&CountActionClient::result_callback, this, _1);
             this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
         }
 
     private:
+        // 创建一个客户
         rclcpp_action::Client<Count>::SharedPtr client_ptr_;
+        // 创建定时器
         rclcpp::TimerBase::SharedPtr timer_;
 
+        // 应答回调函数
         void goal_response_callback(std::shared_future<GoalHandleCount::SharedPtr> future)
         {
+            // 获取应答数据
             auto goal_handle = future.get();
             if (!goal_handle)
             {
+                // 打印被服务器拒绝的消息
                 RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
             }
             else
             {
+                // 打印服务器接收的消息
                 RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
             }
         }
 
+        // 反馈回调函数
         void feedback_callback(
             GoalHandleCount::SharedPtr,
             const std::shared_ptr<const Count::Feedback> feedback)
         {
+            // 打印反馈回来的数据
             RCLCPP_INFO(this->get_logger(), "The progress is: %d%s", feedback->percent_complete, "%");
         }
 
+        // 结果回调函数
         void result_callback(const GoalHandleCount::WrappedResult &result)
         {
+            // switch语句判断结果
             switch (result.code)
             {
             case rclcpp_action::ResultCode::SUCCEEDED:
@@ -105,11 +133,13 @@ namespace my_c_actionclient_node
             ss << "Result received: ";
             ss << " " << result.result->finish;
 
+            // 打印结果回传数据
             RCLCPP_INFO(this->get_logger(), ss.str().c_str());
             rclcpp::shutdown();
         }
-    }; // class CountActionClient
+    };
 
-} // namespace my_c_actionclient_node
+}
 
+// 使用组件创建节点
 RCLCPP_COMPONENTS_REGISTER_NODE(my_c_actionclient_node::CountActionClient)
--- src/my_c_action/src/my_c_actionserver_node.cpp
+++ src/my_c_action/src/my_c_actionserver_node.cpp
@@ -1,3 +1,4 @@
+// 包含头文件
 #include <functional>
 #include <memory>
 #include <thread>
@@ -7,19 +8,27 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
+// 创建my_c_actionserver_node的命名空间
 namespace my_c_actionserver_node
 {
+  // 新建一个类，名称为CountActionServer，继承rclcpp::Node节点
   class CountActionServer : public rclcpp::Node
   {
   public:
+    // Count使用别名my_action::action::Count
+    // GoalHandleCount使用别名rclcpp_action::ServerGoalHandle<Count>
     using Count = my_action::action::Count;
     using GoalHandleCount = rclcpp_action::ServerGoalHandle<Count>;
 
+    // 构造函数
     CountActionServer(const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
         : Node("Count_action_server", options)
     {
+      // 使用std::placeholders命名空间
       using namespace std::placeholders;
 
+      // 创建一个客户，创建Count的服务名称
+      // 并绑定三个句柄handle_goal，handle_cancel，handle_accepted
       this->action_server_ = rclcpp_action::create_server<Count>(
           this->get_node_base_interface(),
           this->get_node_clock_interface(),
@@ -32,44 +41,59 @@ namespace my_c_actionserver_node
     }
 
   private:
+    // 创建一个action服务器
     rclcpp_action::Server<Count>::SharedPtr action_server_;
 
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID &uuid,
         std::shared_ptr<const Count::Goal> goal)
     {
+      // 打印目标信息
       RCLCPP_INFO(this->get_logger(), "Received goal, target is %d", goal->goal_num);
+      // 静态检测，告诉静态代码检测工具程序并非没有处理该函数的返回值，而是确实不需要处理该函数的返回值，不需要再对该处代码作此项检测。
       (void)uuid;
+      // 返回“接受并执行”的消息
       return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
+    // 如果目标被取消
     rclcpp_action::CancelResponse handle_cancel(
         const std::shared_ptr<GoalHandleCount> goal_handle)
     {
+      // 打印取消信息
       RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+      // 静态检测
       (void)goal_handle;
+      // 返回“取消被接受”的消息
       return rclcpp_action::CancelResponse::ACCEPT;
     }
 
     void handle_accepted(const std::shared_ptr<GoalHandleCount> goal_handle)
     {
+      // 使用std::placeholders命名空间
       using namespace std::placeholders;
-      // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+      // 创建一个新的线程，函数名为execute，并开始执行execute()
       std::thread{std::bind(&CountActionServer::execute, this, _1), goal_handle}.detach();
     }
 
     void execute(const std::shared_ptr<GoalHandleCount> goal_handle)
     {
+      // 打印正在执行的消息
       RCLCPP_INFO(this->get_logger(), "Executing goal");
+      // 定义循环时间
       rclcpp::Rate loop_rate(10);
+      // 通过goal_handle，获得目标值
       const auto goal = goal_handle->get_goal();
+      // 创建feedback指针，指针类型为Count::Feedback
       auto feedback = std::make_shared<Count::Feedback>();
+      // 定义feedback->percent_complete的别名为rate_of_progress，使之完全等价
       auto &rate_of_progress = feedback->percent_complete;
+      // 创建result指针，指针类型为Count::Result
       auto result = std::make_shared<Count::Result>();
 
       for (int i = 1; (i <= goal->goal_num) && rclcpp::ok(); ++i)
       {
-        // Check if there is a cancel request
+        // 检查请求有没有被取消
         if (goal_handle->is_canceling())
         {
           result->finish = i;
@@ -77,16 +101,16 @@ namespace my_c_actionserver_node
           RCLCPP_INFO(this->get_logger(), "Goal canceled");
           return;
         }
-        // Update rate_of_progress
+        // 更新进程
         rate_of_progress = 100 * i / goal->goal_num;
-        // Publish feedback
+        // 发布反馈数据
         goal_handle->publish_feedback(feedback);
         RCLCPP_INFO(this->get_logger(), "Publish feedback");
 
         loop_rate.sleep();
       }
 
-      // Check if goal is done
+      // 检测目标是否已经完成
       if (rclcpp::ok())
       {
         result->finish = goal->goal_num;
@@ -94,8 +118,9 @@ namespace my_c_actionserver_node
         RCLCPP_INFO(this->get_logger(), "Goal succeeded");
       }
     }
-  }; // class CountActionServer
+  };
 
-} // namespace action_tutorials_cpp
+}
 
+// 使用组建创建节点
 RCLCPP_COMPONENTS_REGISTER_NODE(my_c_actionserver_node::CountActionServer)
