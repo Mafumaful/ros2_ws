fbf2987af5a8603650ae53b6fe659c2802a91130
mafumaful <2993821152@qq.com>
2022-06-28 20:55:50 +0800

    last

--- src/my_py_action/my_py_action/my_py_actionclient_node.py
+++ src/my_py_action/my_py_action/my_py_actionclient_node.py
@@ -1,3 +1,4 @@
+# 包含头文件
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
@@ -5,38 +6,47 @@ from rclpy.node import Node
 from my_action.action import Count
 
 
-class CountActionClient(Node):
+class CountActionClient(Node):  # 定义一个Client类，继承Node
 
     def __init__(self):
-        super().__init__('Count_action_client')
-        self._action_client = ActionClient(self, Count, 'count')
-        self.send_goal_timer = self.create_timer(0.5, self.send_goal)
+        super().__init__('Count_action_client')  # 初始化节点，名称为Count_action_client
+        self._action_client = ActionClient(self, Count, 'count')  # 服务名绑定为count
+        self.send_goal_timer = self.create_timer(
+            0.5, self.send_goal)  # 创建一个定时器，绑定回调函数send_goal
 
     def send_goal(self):
-        self.send_goal_timer.cancel()
-        goal_msg = Count.Goal()
-        goal_msg.goal_num = 10
-        self._action_client.wait_for_server()
+        # 发送目标
+        self.send_goal_timer.cancel()  # 计时器取消循环
+        goal_msg = Count.Goal()  # goal_msg类型为Count.Goal()
+        goal_msg.goal_num = 10  # 设置目标数字为10
+        self._action_client.wait_for_server()  # 等待服务器
         self.get_logger().info('Sending goal')
+        # 绑定反馈回调函数feedback_cb
         self._send_goal_future = self._action_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_cb)
+        # 绑定应答回调函数goal_response_callback
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
-        """收到目标处理结果"""
+        """收到应答结果"""
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal was rejected by server')
             return
         self.get_logger().info('Goal accepted by server, waiting for result')
         self._get_result_future = goal_handle.get_result_async()
+        # 绑定结果回调函数
+        self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         """获取结果反馈"""
         result = future.result().result
-        self.get_logger().info(f'Result: {result.finish}')
+        # 打印结果内容
+        self.get_logger().info(f'Result received: {result.finish}')
+        rclpy.shutdown()
 
     def feedback_cb(self, future):
+        # 打印反馈内容
         feedback = future.feedback
         self.get_logger().info(
             f'The progress is: {feedback.percent_complete}%')
@@ -46,7 +56,6 @@ def main(args=None):
     rclpy.init(args=args)
     action_client = CountActionClient()
     rclpy.spin(action_client)
-    rclpy.shutdown()
 
 
 if __name__ == '__main__':
--- src/my_py_action/my_py_action/my_py_actionserver_node.py
+++ src/my_py_action/my_py_action/my_py_actionserver_node.py
@@ -1,3 +1,4 @@
+# 包含头文件
 import time
 
 import rclpy
@@ -7,33 +8,39 @@ from rclpy.node import Node
 from my_action.action import Count
 
 
-class CountActionServer(Node):
+class CountActionServer(Node):  # 定义一个Client类，继承Node
 
+    # 构造函数
     def __init__(self):
+        # 创建一个新的节点，节点名称为Count_action_server
         super().__init__('Count_action_server')
         self._action_server = ActionServer(
             self,
             Count,
             'count',
-            self.execute_callback,
-            callback_group=None
+            self.execute_callback
         )
 
-    def execute_callback(self, goal_handle):
+    def execute_callback(self, goal_handle):  # 执行回调函数，当收到目标消息的时候，会进入这个函数
         self.get_logger().info('Received goal, target is %d' %
                                goal_handle.request.goal_num)
 
         self.get_logger().info('Executing goal')
 
+        # 反馈消息为Count.Feedback()类型
         feedback_msg = Count.Feedback()
 
         for i in range(1, goal_handle.request.goal_num+1):
+            # 结果为Count.Result()类型
             result = Count.Result()
 
+            # 给反馈赋值
             feedback_msg.percent_complete = int(
                 100/goal_handle.request.goal_num*i)
+            # 发布反馈处理结果
             goal_handle.publish_feedback(feedback_msg)
             self.get_logger().info('Publish feedback')
+            # 延时函数
             time.sleep(0.1)
 
         if rclpy.ok():
@@ -45,10 +52,9 @@ class CountActionServer(Node):
 
 
 def main(args=None):
+    # 初始化
     rclpy.init(args=args)
-
     Count_action_server = CountActionServer()
-
     rclpy.spin(Count_action_server)
 
 
